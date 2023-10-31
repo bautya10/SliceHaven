e8ac58585725016e7e99bf9a598e19e1c6716c9b
agustintarancon <agustinbuf@gmail.com>
2023-10-31 02:18:18 -0300

    Implementacion navbar

--- src/components/General/NavBar/NavBar.jsx
+++ src/components/General/NavBar/NavBar.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import {customLink, customNavbar} from "./navbar.module.css"
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <nav className={`navbar navbar-expand-lg ${customNavbar}`}>
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand pe-3">SliceHaven</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbar">
+            <ul className="navbar-nav">
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} aria-current="page" href="/">Inicio</a>
+              </li>
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} href="#">Quienes somos</a>
+              </li>
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} href="#">Reservas</a>
+              </li>
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} href="#">Contacto</a>
+              </li>
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} href="#">Cerrar sesion</a>
+              </li>
+              <li className="nav-item pb-1 pe-3">
+                <a className={`nav-link ${customLink}`} href="#">Administracion</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   )
 }
 
