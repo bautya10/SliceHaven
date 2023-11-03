ca185b7e7fd76b63d9f3ae6c016a1ee91527431e
danadiaz <danarodiaz3@gmail.com>
2023-11-02 22:33:11 -0300

    Conflicto solucionado

--- src/components/Specific/GalleryImage/GalleryImage.jsx
+++ src/components/Specific/GalleryImage/GalleryImage.jsx
@@ -6,20 +6,20 @@ import image3 from '../../../assets/imagegallery/image3.jpg';
 
 const GalleryImage = () => {
   return (
-    <div className={`${styles.heightgalery} container-fluid mt-4 align-items-center d-flex`}>
+    <div className={`${styles.HeightGalery} container-fluid mt-4 align-items-center d-flex`}>
       <div className='row'>
         <div className='col-12 col-md-6 col-lg-4 col-xl-4 my-2'>
-          <img src={image1} className={styles.imagegallery} alt="" />
+          <img src={image1} className={styles.ImageGallery} alt="foto de nuestros paltos gourmet" />
         </div>
         <div className='col-12 col-md-6 col-lg-4 col-xl-4 my-2'>
-          <img src={image2} className={styles.imagegallery} alt="" />
+          <img src={image2} className={styles.ImageGallery} alt="foto de nuestros paltos gourmet" />
         </div>
         <div className={`col-12 col-md col-lg-4 col-xl-4 my-2 ${styles.imagenone}`}>
-          <img src={image3} className={styles.imagegallery} alt="" />
+          <img src={image3} className={styles.ImageGallery} alt="foto de nuestros paltos gourmet" />
         </div>
       </div>
     </div>
   )
-}
+};
 
 export default GalleryImage;
