16ab20220e99b40d489d8d20959b0ddf4225a331
Emiliano Augusto Gasco <emilianogasco99@gmail.com>
2023-11-12 18:08:46 -0300

    actalice reservas

--- src/pages/Reservas/Reservas.jsx
+++ src/pages/Reservas/Reservas.jsx
@@ -2,7 +2,7 @@ import Reserves from "../../components/Specific/Reserves/Reserves"
 const Reservas = ({user}) => {
   return (
     <div>
-      <div className=" d-flex justify-content-md-center my-3  ">
+      <div className=" d-flex justify-content-md-center my-3  " id="reservas">
         <div>
           <Reserves user={user}/>
         </div>
