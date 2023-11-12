7dc79b2a4bafec0e32e990b3293de7c14230f2a5
bautistaarias <bautiariaaas@gmail.com>
2023-11-12 18:09:02 -0300

    Actualice login

--- src/components/Specific/registerForm/RegisterForm.jsx
+++ src/components/Specific/registerForm/RegisterForm.jsx
@@ -4,7 +4,6 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from "react-hook-form"
 import axios from 'axios'
 
-
 const RegisterForm = () => {
   const navigate = useNavigate()
 
