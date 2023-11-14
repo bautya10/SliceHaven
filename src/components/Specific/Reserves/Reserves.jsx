e88c54495ba50cb65933e208d475e1a2e1a7e485
Emiliano Augusto Gasco <emilianogasco99@gmail.com>
2023-11-14 12:47:54 -0300

    actualice reservas y la tabla de reservas

--- src/components/Specific/Reserves/Reserves.jsx
+++ src/components/Specific/Reserves/Reserves.jsx
@@ -7,8 +7,7 @@ import axios from 'axios';
 import Alert from "../Alert/Alert"
 registerLocale('es', es);
 
-const Reserves = () => {
-
+const Reserves = ({editar,idUser,idReserva}) => {
   // funcion para borrar las alertas
   const borrarAlerta = () => {
     setTimeout(() => {
@@ -111,6 +110,7 @@ const Reserves = () => {
 
   // esta funcion siver para guardar la reserva
   const guardar = async () => {
+    
     //consulta si de crearReserva existe usuario, si no existe pide logear y si sí hace la peticion
     if (crearReserva.user) {
       try {
@@ -164,6 +164,17 @@ const Reserves = () => {
               icon={'bi bi-exclamation-triangle-fill'}
             />)
           borrarAlerta()
+        }else if(editar){
+          crearReserva.user = idUser
+          // console.log(crearReserva)
+          await axios.patch(`https://slicenhaven-backend.onrender.com/reserves/${idReserva}`, crearReserva);
+          setAlerta(
+            <Alert
+              texto={'Recerva actualizada correctamente'}
+              color={'success'}
+              icon={'bi bi-check-circle-fill'}
+            />)
+          borrarAlerta();
         }
         else {
           //caso contrario, realizo la peticion
@@ -259,7 +270,7 @@ const Reserves = () => {
 
 
       />
-      <button onClick={guardar} className='btn btn-outline-success w-100 mt-3'>Hacer Reserva</button>
+      <button onClick={guardar} className='btn btn-outline-success w-100 mt-3'>{editar? 'Editar Reseva':'Hacer Reserva'}</button>
       <div>{alerta}</div>
     </>
   )
