853c39c1743d55214dbda5f5b74098fadcd1f683
agustintarancon <agustinbuf@gmail.com>
2023-11-26 00:38:40 -0300

    detalles en pagina de error404

--- src/pages/error/Error404.jsx
+++ src/pages/error/Error404.jsx
@@ -1,7 +1,18 @@
+import {containerCustom} from "./error.module.css"
+import { Link } from "react-router-dom"
 
 const Error404 = () => {
   return (
-    <div>Error404</div>
+    <div className={`container d-flex align-items-center justify-content-center ${containerCustom}`}>
+      <div className="row">
+        <div className="d-flex flex-column justify-content-center">
+         <h1 className="display-1">Error404</h1>
+          <Link to="/" className="row justify-content-center text-decoration-none">
+            <button className="btn btn-primary">volver al inicio</button>
+          </Link>
+        </div>
+      </div>
+    </div>
   )
 }
 
