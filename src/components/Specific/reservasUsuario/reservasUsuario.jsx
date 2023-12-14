17b79e0ea4f65d6c455d979a705d4ab48516d746
Emiliano Augusto Gasco <emilianogasco99@gmail.com>
2023-12-14 01:44:41 -0300

    actualice mis reservas

--- src/components/Specific/reservasUsuario/reservasUsuario.jsx
+++ src/components/Specific/reservasUsuario/reservasUsuario.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 import Alert from '../Alert/Alert';
 import Reserves from '../Reserves/Reserves';
 
-const ReserveTable = ({user}) => {
+const ReserveTable = ({ user }) => {
   const [actualizar, setActualizar] = useState(0);
   const [reservas, setReservas] = useState([]);
   const [alerta, setAlerta] = useState(null);
@@ -13,9 +13,9 @@ const ReserveTable = ({user}) => {
   const [idReserva, setIdReserva] = useState();
 
   const idLogued = user?.loguedUser.userFounded.reserves
-    
+
   useEffect(() => {
-  
+
     const obtenerTodasLasReservas = async () => {
       try {
         const result = await axios.get(`https://slicenhaven-backend.onrender.com/reserves/reservesAll`);
@@ -84,8 +84,13 @@ const ReserveTable = ({user}) => {
                 <h5 className="card-title">Fecha: {`${new Date(element.date).getDate()}-${new Date(element.date).getMonth()}-${new Date(element.date).getFullYear()} a ${new Date(element.date).getHours()}:${new Date(element.date).getMinutes()}:${new Date(element.date).getMinutes()} hs.`}</h5>
                 <h5 className="card-text">Personas: {element.people}</h5>
                 <div className="d-flex justify-content-around mt-3">
-                  <button className="btn btn-danger" onClick={() => eliminar(element._id)}><i className="bi bi-trash3"></i></button>
-                  <button className="btn btn-secondary" onClick={() => editar(element.user._id, element._id)} data-bs-toggle="modal" data-bs-target="#staticModalReserva"><i className="bi bi-pencil-square"></i></button>
+                  <button className="btn btn-danger" onClick={() =>
+                    confirm("¿Desea eliminar la reservar?") == true ? eliminar(element._id) : ''}
+
+                  ><i className="bi bi-trash3"></i></button>
+                  <button className="btn btn-secondary" onClick={() => confirm("¿Desea editar la reservar?") == true ? editar(element.user._id, element._id) : ''}
+
+                    data-bs-toggle="modal" data-bs-target="#staticModalReserva"><i className="bi bi-pencil-square"></i></button>
                 </div>
               </div>
             </div>
@@ -101,10 +106,10 @@ const ReserveTable = ({user}) => {
               <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-              <Reserves editar={editarReserva} idUser={idUser} idReserva={idReserva}/>
+              <Reserves editar={editarReserva} idUser={idUser} idReserva={idReserva} />
             </div>
             <div className="modal-footer">
-              <button type="button" onClick={()=> setActualizar(actualizar + 1)} className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+              <button type="button" onClick={() => setActualizar(actualizar + 1)} className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
           </div>
         </div>
